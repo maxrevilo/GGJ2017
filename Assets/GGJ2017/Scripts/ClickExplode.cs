085dc3ec14533a53e564a0c6be8543e8699874d8
ForgeDk <metroidcero@gmail.com>
2017-01-21 10:59:10 -0500

    Ultima version Daniel

--- Assets/GGJ2017/Scripts/ClickExplode.cs
+++ Assets/GGJ2017/Scripts/ClickExplode.cs
@@ -1,27 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ClickExplode : MonoBehaviour {
 	public  Transform PosicionDeLaBola;
 	public Rigidbody2D RigBody;
 	public float MultiplicadorDeFuerza;
 	public float MagnitudDeExplosion;
+	public float TiempoSeleccionado;
+	public float MaximoTiempoDeSeleccion;
+	public GameObject OndaInicial;
 
 	// Use this for initialization
 	void Start () {
 		PosicionDeLaBola = GetComponent<Transform>();
 		RigBody = GetComponent<Rigidbody2D> ();
-		
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
-			Vector2 VectorDeBolaHaPuntero =PosicionDeLaBola.position-Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			print (VectorDeBolaHaPuntero);
+		if(Input.GetButton("Fire1")){
+			activarTiempoDeCargaDeOnda ();
+		}
+		if (Input.GetButtonUp("Fire1")) {
+			desplegarOnda ();
+		}
+	}
 
-			RigBody.AddForce ((MultiplicadorDeFuerza/VectorDeBolaHaPuntero.magnitude)*VectorDeBolaHaPuntero);
-			}
+	public void desplegarOnda(){
+		calcularMagnitudDeExplosion();
+		Vector2 PosicionDelMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		iniciarOnda (PosicionDelMouse);
+		TiempoSeleccionado = 0f;
+	}
+
+	public void iniciarOnda(Vector2 PosicionDeGeneracion){
+		GameObject OndaGenerada = Instantiate (OndaInicial);
+		OndaGenerada.transform.position = PosicionDeGeneracion;
+		Onda ScriptHonda = OndaGenerada.GetComponent<Onda> ();
+		ScriptHonda.setRadioDeOnda (2.5f);
+	    ScriptHonda.SetFuerzaDeOnda (MagnitudDeExplosion);
+	}
+
+	public void activarTiempoDeCargaDeOnda(){
+		TiempoSeleccionado += Time.deltaTime;
+	}
+
+	public void calcularMagnitudDeExplosion(){
+		if (TiempoSeleccionado <= MaximoTiempoDeSeleccion) {
+			float FraccionDeFuezaAlcanzada = (TiempoSeleccionado / MaximoTiempoDeSeleccion);
+			print (FraccionDeFuezaAlcanzada);
+			MagnitudDeExplosion = MultiplicadorDeFuerza * FraccionDeFuezaAlcanzada;
+		} else {
+			MagnitudDeExplosion = MultiplicadorDeFuerza;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D ObjetoQueColiciono){
+		string TagDelCollider = ObjetoQueColiciono.gameObject.tag;
+		if(TagDelCollider=="ObstaculoAsesino"){
+			print ("GameOver");
+			NivelPerdido ();
+		}
+	}
+
+	void OnBecameInvisible(){
+		NivelPerdido ();
+	}
+
+	public void NivelPerdido(){
+		string IndiceEscenaHaCargar = "PruebaEffectos";
+		SceneManager.LoadScene (IndiceEscenaHaCargar);
 	}
 }
---
+++ Assets/GGJ2017/Scripts/Onda.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onda : MonoBehaviour {
+	public float TiempoAntesDeDestruir;
+	public float RadioDeOnda;
+	public float FuerzaDeOnda;
+	public float RadioInicial;
+	public float MaximoRadio = 2.5f;
+	public CircleCollider2D ColisionadorDelObjeto;
+
+
+	// Use this for initialization
+	void Start () {
+		RadioInicial = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AmpliarRadio ();
+		
+	}
+
+	public void destruirOnda(){
+		Destroy (gameObject);
+	}
+
+	public void setRadioDeOnda(float RadioDeOnda){
+		this.RadioDeOnda = RadioDeOnda;
+	}
+
+	public void SetFuerzaDeOnda (float FuerzaDeOnda){
+		this.FuerzaDeOnda = FuerzaDeOnda;
+	}
+
+	public void AmpliarRadio(){
+		if(ColisionadorDelObjeto.radius <= MaximoRadio){
+			ColisionadorDelObjeto.radius += 0.15f;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D ColiderObjetivo){
+		if(ColiderObjetivo.CompareTag("Player")){
+			print ("Te golpeo una onda");
+			Vector2 PosicionDeLaOnda =  gameObject.transform.position;
+			Vector2 PosicionDelObjeto = ColiderObjetivo.transform.position;
+			Vector2 VectorOndaHaObjeto = PosicionDelObjeto - PosicionDeLaOnda;
+			Rigidbody2D RigidBodyObjetivo = ColiderObjetivo.gameObject.GetComponent<Rigidbody2D> ();
+			RigidBodyObjetivo.AddForce ((FuerzaDeOnda/VectorOndaHaObjeto.magnitude)*VectorOndaHaObjeto);
+		}
+	}
+
+	public void DestruirOnda(){
+		Invoke ("destruirOnda",5f);
+	}
+}
