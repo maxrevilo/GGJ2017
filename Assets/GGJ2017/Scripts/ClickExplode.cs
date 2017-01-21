cf09183b1fa791d685252fd39eb113cce4ab6f93
ForgeDk <metroidcero@gmail.com>
2017-01-20 23:58:15 -0500

    Actualizacion Codigo

--- Assets/GGJ2017/Scripts/ClickExplode.cs
+++ Assets/GGJ2017/Scripts/ClickExplode.cs
@@ -7,20 +7,50 @@ public class ClickExplode : MonoBehaviour {
 	public Rigidbody2D RigBody;
 	public float MultiplicadorDeFuerza;
 	public float MagnitudDeExplosion;
+	public float TiempoSeleccionado;
+	public float MaximoTiempoDeSeleccion;
 
 	// Use this for initialization
 	void Start () {
 		PosicionDeLaBola = GetComponent<Transform>();
 		RigBody = GetComponent<Rigidbody2D> ();
-		
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
-			Vector2 VectorDeBolaHaPuntero =PosicionDeLaBola.position-Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			print (VectorDeBolaHaPuntero);
-			RigBody.AddForce ((MultiplicadorDeFuerza/VectorDeBolaHaPuntero.magnitude)*VectorDeBolaHaPuntero);
+		if(Input.GetButton("Fire1")){
+			activarTiempoDeCargaDeOnda ();
+		}
+		if (Input.GetButtonUp("Fire1")) {
+			desplegarOnda ();
 			}
+	}
+
+	public void desplegarOnda(){
+		calcularMagnitudDeExplosion();
+		Vector2 VectorDeClickHaObjeto =PosicionDeLaBola.position-Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		RigBody.AddForce ((MagnitudDeExplosion/VectorDeClickHaObjeto.magnitude)*VectorDeClickHaObjeto);
+		TiempoSeleccionado = 0f;
+	}
+
+	public void activarTiempoDeCargaDeOnda(){
+		TiempoSeleccionado += Time.deltaTime;
+	}
+
+	public void calcularMagnitudDeExplosion(){
+		if (TiempoSeleccionado <= MaximoTiempoDeSeleccion) {
+			float FraccionDeFuezaAlcanzada = (TiempoSeleccionado / MaximoTiempoDeSeleccion);
+			print (FraccionDeFuezaAlcanzada);
+			MagnitudDeExplosion = MultiplicadorDeFuerza * FraccionDeFuezaAlcanzada;
+		} else {
+			MagnitudDeExplosion = MultiplicadorDeFuerza;
+		}
+	}
+
+	 void OnTriggerEnter2D(Collider2D ColliderConElQueColisiono){
+		string TagDelCollider = ColliderConElQueColisiono.tag;
+		if(TagDelCollider=="ObstaculoAsesino"){
+			print ("GameOver");
+		}
 	}
 }
