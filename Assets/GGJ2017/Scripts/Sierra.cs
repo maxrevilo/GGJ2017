99165c24f12ba161c9e387a53b538bb3885a6c99
Oliver Perez Camargo <oliver.a.perez.c@gmail.com>
2017-01-22 11:44:42 -0500

    Adjustments to level 0

---
+++ Assets/GGJ2017/Scripts/Sierra.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sierra : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D ObjetoQueColiciono){
+		string TagDelCollider = ObjetoQueColiciono.gameObject.tag;
+		if (TagDelCollider == "Player") {
+			ObjetoQueColiciono.gameObject.SendMessage("Epilepsia", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
